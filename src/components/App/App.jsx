4e712495bcb717a888fbcaebd63d10033b7c8a6e
Edgard Finotti Neto <edgardfinotti@yahoo.com.br>
2020-09-13 22:17:55 -0300

    Create statistic component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,14 +1,28 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import  { Wrapper, Container } from './App.styles'
 import AppHeader from '../AppHeader'
 import AppContainer from '../AppContainer'
 import CheckBox from '../../shared/CheckBox/CheckBox'
+import LineChart from '../../shared/LineChart'
 
 function App () {
 
     /* criar o estado: */
     const [lettuce, setLettuce] = useState(true) /* get e set, true = inicial */
     const [rice, setRice] = useState(false)
+    /* const [healthy, setHealthy] = useState(20) */
+    
+    const colors = ['#62CBC6', '#00ABAD', '#00858C', '#006073', '#004D61']
+
+    /* na criação do compente executa a função */
+    /* executar depois de 5 segundos */
+    /*
+    useEffect(function () { 
+        setTimeout(() => { 
+            setHealthy(80)
+        }, 5000)
+    })
+    */
 
     return <Wrapper>
 
@@ -38,6 +52,20 @@ function App () {
                 </div>}
                 right={<div>
                     estatisticas
+
+                    <LineChart 
+                        color={colors[0]} title="Saudável" percentage={80}
+                    />
+                    <LineChart 
+                        color={colors[1]}title="Não tão Saudável" percentage={20}
+                    />
+                    <LineChart 
+                        color={colors[2]}title="Limpeza" percentage={35}
+                    />
+                    <LineChart 
+                        color={colors[3]}title="Outros" percentage={15}
+                    />
+                    
                 </div>}
             />
            
---
+++ src/shared/LineChart/LineChart.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Wrapper, ProgressBar } from './LineChart.styles'
+
+function LineChart ({ title, percentage, color  }) {
+  return  <Wrapper>
+
+    <span>
+      {title}:
+
+    </span>
+    <ProgressBar 
+      percentage={percentage}
+      color={color}
+    />
+
+  </Wrapper>
+}
+
+export default LineChart
